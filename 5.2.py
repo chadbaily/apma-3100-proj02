3a3ba8170347d55e333d3907fc044c31530c5b3b
Chad <chadbaily27@gmail.com>
2018-04-25 20:53:11 -0400

    Done for n

--- 5.2.py
+++ 5.2.py
@@ -44,71 +44,69 @@ for n in mN:
         results.append(
             (n, zscore(sum([pair[1] for pair in tempResults]) / n, n)))  # M_n
 
-ycord = [pair[1] for pair in results]
-print("Average: ", np.mean(ycord))
-print("Sqrt of Standard Deviation: ", math.sqrt(np.std(ycord)))
+    ycord = [pair[1] for pair in results]
+    # print("Average: ", np.mean(ycord))
+    # print("Sqrt of Standard Deviation: ", math.sqrt(np.std(ycord)))
 
+    points = [0, 0, 0, 0, 0, 0, 0]
+    zValues = [1.4, 1, 0.5, 0, -0.5, -1, -1.4]
 
-points = [0, 0, 0, 0, 0, 0, 0]
-zValues = [1.4, 1, 0.5, 0, -0.5, -1, -1.4]
+    for point in ycord:
+        if(point <= 1.4):
+            points[0] += 1
+        if(point <= 1.0):
+            points[1] += 1
+        if(point <= 0.5):
+            points[2] += 1
+        if(point <= 0):
+            points[3] += 1
+        if(point <= -0.5):
+            points[4] += 1
+        if(point <= -1):
+            points[5] += 1
+        if(point <= -1.4):
+            points[6] += 1
 
-for point in ycord:
-    if(point <= 1.4):
-        points[0] += 1
-    if(point <= 1.0):
-        points[1] += 1
-    if(point <= 0.5):
-        points[2] += 1
-    if(point <= 0):
-        points[3] += 1
-    if(point <= -0.5):
-        points[4] += 1
-    if(point <= -1):
-        points[5] += 1
-    if(point <= -1.4):
-        points[6] += 1
+    for i in range(0, len(points)):
+        points[i] = points[i] / 880  # getting probability
 
-for i in range(0, len(points)):
-    points[i] = points[i] / 880  # getting probability
+    empericalCDF = []
+    for i in range(0, len(points)):
+        print("The probability of " +
+              str(zValues[i]) + " is: " + str(points[i]))
+        empericalCDF.append((zValues[i], points[i]))
 
-empericalCDF = []
-for i in range(0, len(points)):
-    print("The probability of " + str(zValues[i]) + " is: " + str(points[i]))
-    empericalCDF.append((zValues[i], points[i]))
+    madnValues = []
+    cdfValue = [0.9192, 0.8413, 0.6915, 0.5, 0.3085, 0.1587, 0.0808]
+    for i in range(0, len(points)):
+        madnValues.append(np.abs(points[i] - cdfValue[i]))
 
+    MADn = np.amax(madnValues)
+    MADnPlot = (zValues[np.argmax(madnValues)], MADn)
+    print("The max value for MADn = " + str(MADn))
 
-madnValues = []
-cdfValue = [0.9192, 0.8413, 0.6915, 0.5, 0.3085, 0.1587, 0.0808]
-for i in range(0, len(points)):
-    madnValues.append(np.abs(points[i] - cdfValue[i]))
+    # Calculate the z score for -2.5 to 2.5
+    # z = []
+    # cdf = []
+    plotCDF = []
+    start = -2.5
+    while start <= 2.5:
+        # cdf.append(st.norm.cdf(start))
+        # z.append(start)
+        plotCDF.append((start, st.norm.cdf(start)))
+        start += 0.01
 
-MADn = np.amax(madnValues)
-MADnPlot = (zValues[np.argmax(madnValues)], MADn)
-print("The max value for MADn = " + str(MADn))
+    # for i in range(0, len(z)):
+    #     print("The z score for " + str(z[i]) + " is " + str(cdf[i]))
 
-# Calculate the z score for -2.5 to 2.5
-# z = []
-# cdf = []
-plotCDF = []
-start = -2.5
-while start <= 2.5:
-    # cdf.append(st.norm.cdf(start))
-    # z.append(start)
-    plotCDF.append((start, st.norm.cdf(start)))
-    start += 0.01
-
-# for i in range(0, len(z)):
-#     print("The z score for " + str(z[i]) + " is " + str(cdf[i]))
-
-
-plt.title('')
-plt.plot(*zip(*plotCDF))
-plt.scatter(*zip(*empericalCDF))
-plt.scatter(*MADnPlot)
-# plt.axhline(np.mean(ycord), color='black')
-plt.ylabel('mn')
-plt.xlabel('n')
-plt.show()
+    plt.title('Plot for ' + str(n))
+    plt.plot(*zip(*plotCDF))
+    plt.scatter(*zip(*empericalCDF))
+    plt.scatter(*MADnPlot)
+    # plt.axhline(np.mean(ycord), color='black')
+    plt.ylabel('mn')
+    plt.xlabel('n')
+    plt.show()
 
 # Print out all results
 # count = 0
